40ec75d76d6d5efad0944aee602dafd7778934c0
lyanedev <lyane.lamara@gmail.com>
2022-02-28 12:26:07 +0100

    🚀 enhaced UI todo card

--- src/components/Todoslist.jsx
+++ src/components/Todoslist.jsx
@@ -3,8 +3,11 @@ import { NavLink } from "react-router-dom";
 import { styled } from "@mui/material/styles";
 import { Card, Typography, Button, Grid } from "@mui/material";
 import ErrorRoundedIcon from "@mui/icons-material/ErrorRounded";
+import SentimentDissatisfiedRoundedIcon from "@mui/icons-material/SentimentDissatisfiedRounded";
+import SentimentSatisfiedAltRoundedIcon from "@mui/icons-material/SentimentSatisfiedAltRounded";
 
 import "./todosList.styled";
+import { Box } from "@mui/system";
 
 const TodoCard = styled(Card)({
   maxWidth: 350,
@@ -17,43 +20,76 @@ const TodoCard = styled(Card)({
 
 export const Todoslist = ({ todos }) => {
   return (
-    <>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        flexWrap: "wrap",
+        padding: 5,
+      }}
+    >
       {todos.map((todo) => (
         <TodoCard key={todo.id}>
           <Grid
             container
             direction="row"
-            justifyContent="space-around"
-            alignItems="flex-start"
+            justifyContent="flex-start"
+            rowSpacing={2}
           >
-            <Grid item xs={12}>
+            <Grid item>
               <Typography variant="h4">{todo.title}</Typography>
             </Grid>
+            <Grid
+              container
+              direction="row"
+              justifyContent="flex-start"
+              alignItems="center"
+            >
+              <Typography variant="p">Tags: </Typography>
+              {todo.tags.map((tag) => (
+                <Box
+                  sx={{
+                    display: "inline-box",
+                    outline: "2px solid #7b1fa2",
+                    background: "#ce93d8",
+                    borderRadius: 20,
+                    padding: "0.1rem 0.5rem",
+                    textAlign: "center",
+                    margin: "0.5rem .3rem",
+                    color: "#7b1fa2",
+                  }}
+                >
+                  {tag}
+                </Box>
+              ))}
+            </Grid>
+
+            {todo.important && (
+              <>
+                <Grid item xs={1}>
+                  <ErrorRoundedIcon sx={{ color: "#f44336" }} />
+                </Grid>
+                <Grid item xs={11}>
+                  <Typography variant="p" sx={{ color: "#f44336" }}>
+                    Important
+                  </Typography>
+                </Grid>
+              </>
+            )}
             <Grid item xs={12}>
-              <Typography variant="p">Tags: {todo.tags}</Typography>
+              <Typography variant="p">
+                {todo.description.substring(0, 60)}...
+              </Typography>
             </Grid>
-            <Grid item xs={1}>
-              {todo.important && <ErrorRoundedIcon sx={{ color: "#f44336" }} />}
+
+            <Grid>
+              <NavLink to={`/todos/${todo.id}`}>
+                <Button variant="text">See more</Button>
+              </NavLink>
             </Grid>
-            <Grid item xs={11}>
-              {todo.important && (
-                <Typography variant="p" sx={{ color: "#f44336" }}>
-                  Important
-                </Typography>
-              )}
-            </Grid>
-            <Typography variant="p">
-              {todo.done ? "Done" : "Not done"}
-            </Typography>
-            <Typography variant="p">
-              {todo.description.substring(0, 60)}...
-            </Typography>
-            <NavLink to={`/todos/${todo.id}`}>
-              <Button variant="outlined">See more</Button>
-            </NavLink>
           </Grid>
         </TodoCard>
       ))}
-    </>
+    </Box>
   );
 };
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -8,7 +8,7 @@ export const Home = () => {
   const { data, isPending, error } = useFetch("http://localhost:3000/todos");
 
   return (
-    <Container maxWidth="xl" sx={{padding: 5}}>
+    <Container maxWidth="xl">
       {error && <Typography variant="p">{error}</Typography>}
       {isPending && <Typography variant="p">Loading...</Typography>}
       {data && <Todoslist todos={data}/>}
